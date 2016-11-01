1a41c153e9ee24bb0662830d4f8deeac3e53b51f
hodd <hodd@gmx.com>
2016-11-01 10:17:34 +0100

    app

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ server.listen(8080);
 // Express
 
 app.use(express.static(__dirname)); // directory of currently executing script
-app.use(express.static(__dirname + '/public')); // using static files in public folder (css, img)
+app.use(express.static(__dirname + '/public')); // use static files in public folder (css, img)
 
 app.get('/', function (req, res) { // GET request homepage
 	res.sendFile(__dirname + '/index.html'); // respond index.html
@@ -33,49 +33,12 @@ T.get('search/tweets', param, function(err, data, response) {
 	var tweets = data.statuses;
 	//console.log(tweets);
 	for (var i = 0; i < tweets.length; i++) {
-		console.log(tweets[i].text)+'\n';
-		io.emit('tweet', tweets[i].text); // sending twitter event
+		console.log(i +'/ '+ tweets[i].text)+'\n';
+		io.emit('tweet', tweets[i].text); // send twitter event
 	}
 
 });
 
-
-// poster un tweet
-
-/*
-function postTweet() {
-
-	var newTweet = {
-		status: 'hello from node.js !'
-	}
-
-	T.post('statuses/update', newTweet, tweeted);
-
-	function tweeted (err, data, response) {
-		if (err) {
-			console.log("try again");
-		}else {
-			console.log("yeah");
-		}
-	}
-}
-*/
-
-
-// citer @ user
-
-/*
-var stream = T.stream('user');
-
-// dès que quelqu'un me suit
-stream.on('follow', followed);
-
-function followed(eventMsg) {
-	var name = eventMsg.source.name;
-	var screenName = eventMsg.source.screen_name;
-	postTweet('@' + screenName + 'Thank you for following me !');
-}
-*/
 
 
 
